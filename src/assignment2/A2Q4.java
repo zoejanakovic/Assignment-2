6ed4720c0e814fc8c1386d3230eb7ac989996904
janaz9178 <janaz9178@HRH-IC0027337.wrdsb.ca>
2017-02-17 10:12:42 -0500

    first commit

---
+++ src/assignment2/A2Q4.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Wall;
+
+/**
+ *
+ * @author janaz9178
+ */
+public class A2Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //creating city
+        City kw = new City();
+        //building castle
+        new Wall(kw, 1, 1, Direction.NORTH);
+        new Wall(kw, 1, 1, Direction.EAST);
+        new Wall(kw, 1, 1, Direction.SOUTH);
+        new Wall(kw, 1, 1, Direction.WEST);
+        new Wall(kw, 2, 2, Direction.NORTH);
+        new Wall(kw, 2, 2, Direction.WEST);
+        new Wall(kw, 2, 3, Direction.NORTH);
+        new Wall(kw, 2, 3, Direction.EAST);
+        new Wall(kw, 3, 2, Direction.WEST);
+        new Wall(kw, 3, 2, Direction.SOUTH);
+        new Wall(kw, 3, 3, Direction.SOUTH);
+        new Wall(kw, 3, 3, Direction.EAST);
+        new Wall(kw, 4, 1, Direction.NORTH);
+        new Wall(kw, 4, 1, Direction.WEST);
+        new Wall(kw, 4, 1, Direction.SOUTH);
+        new Wall(kw, 4, 1, Direction.EAST);
+        new Wall(kw, 4, 4, Direction.NORTH);
+        new Wall(kw, 4, 4, Direction.WEST);
+        new Wall(kw, 4, 4, Direction.SOUTH);
+        new Wall(kw, 4, 4, Direction.EAST);
+    }
+}
